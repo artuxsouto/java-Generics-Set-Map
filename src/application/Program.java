87d07a61a4923b8c571722653a74b52e35851fad
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-04-21 17:22:40 -0300

    Problema com aplicação Generics

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
+import mode.entities.PrintService;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
 		
+		Scanner sc = new Scanner(System.in);
+		PrintService ps = new PrintService();
+		
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		
+		for (int i = 1; i <= n; i++) {
+			int value = sc.nextInt();
+			ps.addValue(value);
+		}
+		
+		ps.print();
+		System.out.println("First: " + ps.first());
+		
+		sc.close();
 	}
-
 }
---
+++ src/model/entities/PrintService.java
@@ -0,0 +1,16 @@
+package model.entities;
+
+public class PrintService {
+	
+	public void addValue(int value) {
+		
+	}
+	
+	public int first() {
+		return 0;
+	}
+	
+	public void print() {
+		
+	}
+}
