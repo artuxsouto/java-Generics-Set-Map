53f6107a608033b73460d70f5c3110e06d84cbb4
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-04-22 11:23:02 -0300

    Exerc fix com Set<T> Resolvido

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,45 +1,47 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.time.Instant;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-
-import entities.LogEntry;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		Set<Integer> a = new HashSet<>();
+		Set<Integer> b = new HashSet<>();
+		Set<Integer> c = new HashSet<>();
 		
-		System.out.print("Enter file full path: ");
-		String path = sc.nextLine();
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
-			Set<LogEntry> set = new HashSet<>();
-			
-			String line = br.readLine();
-			while (line != null) {
-			
-				String[] fields = line.split(" ");
-				String username = fields[0];
-				Date moment = Date.from(Instant.parse(fields[1]));
-				
-				set.add(new LogEntry(username, moment));
-				
-				line = br.readLine();
-			}	
-			System.out.println("Total users: " + set.size());
-
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			int code = sc.nextInt();
+			a.add(code);
 		}
+
+		System.out.print("How many students for course B? ");
+		n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			int code = sc.nextInt();
+			b.add(code);
+		}
+		
+		System.out.print("How many students for course C? ");
+		n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			int code = sc.nextInt();
+			c.add(code);
+		}
+		
+		Set<Integer> total = new HashSet<>(a);
+		total.addAll(b);
+		total.addAll(c);
+		
+		System.out.println("Total students: " + total.size());
+		
 		sc.close();
 	}
 }
